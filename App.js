6741a34134e8e24f7fb90de4d384892e64842a91
devlina.das <devlina.das@accenture.com>
2024-12-19 14:45:18 +0530

    first learning lesson

---
+++ App.js
@@ -0,0 +1,10 @@
+let element = document.createElement('h1');
+element.innerHTML='HELLO Javascript ! Tag injected using vannila JS';
+document.getElementById('root').appendChild(element)
+
+
+
+let reactElement = React.createElement('h1',{id:'paraOne',style:{color:'red'}},"Hello World! Tag injected using React CDN");
+let root = ReactDOM.createRoot(document.getElementById('rootTwo'));
+console.log('root',root)
+root.render(reactElement)
